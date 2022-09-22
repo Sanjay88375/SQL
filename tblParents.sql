566dc579da4feb9ea5313aa5177f70e78e430a16
Sanjay88375 <110672235+Sanjay88375@users.noreply.github.com>
2022-09-22 17:08:09 +0530

    Update tblParents.sql

--- tblParents.sql
+++ tblParents.sql
@@ -1,4 +1,7 @@
-Create table tblParents (parent_id int, parents_name varchar(50), city varchar(20), age int, annual_income int, occupation varchar(50), email_id varchar(80));
+/* Program one and 2 Creating a table and insering 20 rows*/
+Create Database Student;
+use Student;
+Create table tblParents (parent_id int Primary key, parents_name varchar(50), city varchar(20), age int, annual_income int, occupation varchar(50), email_id varchar(80));
 Insert into tblParents values (1001, 'Raj', 'Mohali', 45, 2900000, 'Software engineer', 'N/A');
 Insert into tblParents values (1002, 'Ashwin', 'Delhi', 48, 800000, 'Elecrtical engineer', 'N/A');
 Insert into tblParents values (1003, 'Sham', 'Mumabi', 51, 2000000, 'Software engineer', 'N/A');
@@ -20,5 +23,25 @@ Insert into tblParents values (2008, 'Kajal', 'Ahmedabad', 44, 290000, 'Software
 Insert into tblParents values (2009, 'Kira', 'Delhi', 45, 800000, 'Software engineer', 'N/A');
 Insert into tblParents values (2010, 'Ravinder', 'Ahmedabad', 53, 800000, 'Software engineer', 'N/A');
 Select * from tblParents;
+
+/* Program 3 count salary*/
 Select COUNT(annual_income) from tblParents where annual_income >600000;
+
+/*Program 4 Check parents city*/
 Select * from tblParents where city IN ('Delhi','Ahmedabad', 'Mumabi','Banglore','Chennai');
+
+/*Program 5 salary less than 5 lakhs or age more than 45*/
+Select * from tblParents where annual_income <500000 or age >45;
+
+/* Program6 with foreign key*/
+Create table student (parent_id int, student_name varchar(50), maths_marks int, science_marks int, foreign key (parent_id) references tblParents(parent_id));
+Insert into student values (1001, 'Sow', 55, 74);
+Insert into student values (2002, 'Row', 76, 84);
+Insert into student values (2005, 'Powth', 63, 44);
+Insert into student values (2010, 'Parth', 90, 74);
+Insert into student values (1005, 'Paras', 40, 64);
+Insert into student values (1004, 'Pinku', 54, 73);
+Insert into student values (1003, 'Peter', 75, 95);
+Insert into student values (2002, 'Pinki', 85, 96);
+Insert into student values (1008, 'Puppy', 58, 58);
+Select * from student;
